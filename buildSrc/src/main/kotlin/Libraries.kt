46271ea750e41e1e2e30b804698f83b99b68e4fe
monun <monun1010@gmail.com>
2023-08-14 06:23:46 +0900

    주석 처리 코드 제거

--- buildSrc/src/main/kotlin/Libraries.kt
+++ buildSrc/src/main/kotlin/Libraries.kt
@@ -12,38 +12,4 @@ object Libraries {
     object Paper {
         const val Version = "1.20.1"
     }
-
-//    private val _dependencies = linkedSetOf<Dependency>()
-//    val dependencies: Set<Dependency> = _dependencies
-//
-//    fun add(dependency: Dependency) {
-//        if (_dependencies.none { it.contentEquals(dependency) }) {
-//            _dependencies.add(dependency)
-//        }
-//    }
-//
-//    override fun toString(): String {
-//        return _dependencies.joinToString(separator = "\n  ") { dependency ->
-//            val group = dependency.group ?: error("group is null")
-//            var name = dependency.name ?: error("name is null")
-//            var version = dependency.version
-//
-//            if (group == "org.jetbrains.kotlin") {
-//                if (version == null) version = Kotlin.Version
-//            } else if (group == "io.github.monun") { // If you don't like it, delete it. It's for my library.
-//                if (name.endsWith("-api")) name = name.removeSuffix("api") + "core"
-//            }
-//
-//            requireNotNull(version) { "version is null" }
-//            require(version != "latest.release") { "version is latest.release" }
-//
-//            "- $group:$name:$version"
-//        }
-//    }
 }
-
-//fun Dependency?.library() {
-//    if (this != null) {
-//        Libraries.add(this)
-//    }
-//}
